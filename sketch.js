6003729e91418fbb2528cf9013b9dcd2b8b0ae4a
Nick <128640651+nichbar123@users.noreply.github.com>
2026-02-26 12:01:11 -0500

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,38 @@
+let combos;
+
+function preload() {
+  combos = loadJSON("combo_genome.json");
+}
+
+function setup() {
+  createCanvas(windowWidth, combos.length * 20);
+  noLoop();
+}
+
+function draw() {
+  background(15);
+
+  let rowHeight = 18;
+  let boxWidth = 15;
+
+  combos.forEach((combo, i) => {
+    combo.categories.forEach((cat, j) => {
+
+      fill(colorFor(cat));
+      rect(50 + j * boxWidth,
+           i * rowHeight,
+           boxWidth - 2,
+           rowHeight - 2);
+    });
+  });
+}
+
+function colorFor(cat) {
+  switch(cat) {
+    case "normal": return "#4ade80";
+    case "special": return "#60a5fa";
+    case "assist": return "#facc15";
+    case "super": return "#f87171";
+    default: return "#a1a1aa";
+  }
+}
